d484efb7dc0eae12236a5c70bdf392d6a4b84310
Chris Le <chrisvle@berkeley.edu>
2016-02-12 17:36:41 -0800

    Updating riverene and adding photos for viderent

--- riverene/js/custom.js
+++ riverene/js/custom.js
@@ -108,10 +108,10 @@ $('.magnific-all').each(function() {
 
         navigation: false, // Show next and prev buttons
         slideSpeed: 300,
-        paginationSpeed: 800,
+        paginationSpeed: 200,
         singleItem: true,
         transitionStyle: "fade",
-        autoPlay: false,
+        autoPlay: true,
         navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
 
     });
